a0407f0d7a60f7c05d9a2d38797aeed3307fd6cd
fevagg <paingrille19@gmail.com>
2021-11-02 22:04:09 -0300

    async updating for previous searches

--- js/previous.js
+++ js/previous.js
@@ -3,7 +3,7 @@ let visitedCities = localStorage.getItem('visited') !== null ? JSON.parse(localS
 function handlerExit(){
     const btnExit = document.getElementsByClassName('exit');
     for(let btn of btnExit){
-        btn.addEventListener('click', (event)=>{
+        btn.addEventListener('click', async (event)=>{
             event.target.parentElement.remove();
             const cityToDelete = visitedCities.findIndex(city => city.city === event.target.parentElement.children[1].innerHTML);
             visitedCities.splice(cityToDelete, 1);
@@ -15,18 +15,19 @@ function handlerExit(){
 function handlerWeather(){
     const previous = document.getElementsByClassName('previous');
     for(let prev of previous){
-        prev.addEventListener('click', (event)=>{
+        prev.addEventListener('click', async (event)=>{
             const cityToSelect = visitedCities.find(city => city.city === event.target.parentElement.children[1].innerHTML);
-            document.getElementById('app').innerHTML = viewWeather(cityToSelect);
+            const citySelected = await weatherAPI(cityToSelect.city, cityToSelect.country_code);
+            viewWeather(citySelected);
             document.getElementById('exit').addEventListener('click', ()=>{
-                        document.getElementById('app').innerHTML = '';
-                    });
+                document.getElementById('app').innerHTML = '';
+            });
         });
     }
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementById('visited').innerHTML = previousSearches(visitedCities);
+    previousSearches(visitedCities);
     handlerExit();
     handlerWeather();
 });
